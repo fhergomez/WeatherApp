ea17c7116f9770e5fec174ff584b533499c1f2a5
fhergomez <fhergomez37@gmail.com>
2017-02-13 15:56:55 -0800

    work on js api

--- script/script.js
+++ script/script.js
@@ -1,30 +1,41 @@
 $(document).ready(function () {
-  console.log("I'm here!");
+  console.log("I'm here");
 
-  $("#searchtext").keyup(function () {
-      getAutoCompleteValues($("#searchtext").first().attr("value"));
-  });
+  var searchText = $('#city');
+  var weatherDisplay = $('#weatherResults');
 
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
 
-  function getAutoCompleteValues(val) {
+  $('#search').on('click', function(e) {
+    e.preventDefault();
+    console.log('form submitted');
+    weatherDisplay.html('loading......');
+    var url = "http://api.openweathermap.org/data/2.5/weather";
+    if (isNumeric(searchText.q)) {
+      var data = {
+        zip: searchText.val() + ',us',
+        units: 'imperial',
+        APPID: '77ffd60dd230258b705c8d658f5e0e70'
+      };
+    } else {
+      var data = {
+        q: searchText.val(),
+        units: 'imperial',
+        APPID: '77ffd60dd230258b705c8d658f5e0e70'
+      };
+    }
 
-    if (val.length < 3) return false;
-    $.ajax({
-      type: "GET",
-      dataType: "jsonp",
-      jsonp: "callback",
-      jsonpCallback: "callback",
-      url: "http://api.openweathermap.org/data/2.5/forecast?q="+ val +",us&mode=json&units=metric&APPID=77ffd60dd230258b705c8d658f5e0e70",
-      cache: false,
-      success: function (data) {
-        console.log(data);
-        $("#results").html('');
-        $.each(data, function (i, item) {
-           $("#results").append(city.id);
-        });
+    console.log(searchText);
+    // if (isNumeric(data.q)) {
+    //   url = "http://api.openweathermap.org/data/2.5/weather" + data.zip;
+    // }
+    $.getJSON(url, data, function(weatherData) {
+      console.log(url, data, weatherData);
+      if (weatherData.list) {
+        weatherDisplay.html("");
       }
-    });
-  };
-
-  getAutoCompleteValues("Gustine");
+    })
+  })
 });
