47372ef14e7bb763a92a3c4fbfd4b095ba5e29df
fhergomez <fhergomez37@gmail.com>
2017-02-16 22:39:03 -0800

    work on js api

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 
   var searchText = $('#city');
   var weatherDisplay = $('#weatherResults');
+  var apiKey = '77ffd60dd230258b705c8d658f5e0e70'
+  var url = "http://api.openweathermap.org/data/2.5/weather";
 
   function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
@@ -12,17 +14,19 @@ $(document).ready(function () {
     e.preventDefault();
     console.log('form submitted');
     weatherDisplay.html('loading......');
-    var url = "http://api.openweathermap.org/data/2.5/weather";
-
 
     console.log(searchText);
     // if (isNumeric(data.q)) {
     //   url = "http://api.openweathermap.org/data/2.5/weather" + data.zip;
     // }
-    var getCurrentWeather = function () {
-      $.getJSON(url, function (data) {
+
+  });
+
+  var getCurrentWeather = function () {
+    $.getJSON(
+      url,
+      function (data) {
         console.log(data);
-      });
-    }
-  })
+    });
+  }
 });
