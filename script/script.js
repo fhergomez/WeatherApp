4df18972527f04a15095b52bd2619b6d6e2aaec9
fhergomez <fhergomez37@gmail.com>
2017-02-17 01:44:34 -0800

    got api to work

--- script/script.js
+++ script/script.js
@@ -1,32 +1,44 @@
 $(document).ready(function () {
   console.log("I'm here");
 
-  var searchText = $('#city');
+  var searchText = $('#search-text');
+  var q = searchText.val();
   var weatherDisplay = $('#weatherResults');
   var apiKey = '77ffd60dd230258b705c8d658f5e0e70'
-  var url = "http://api.openweathermap.org/data/2.5/weather";
+  var url = "http://api.openweathermap.org/data/2.5/forecast";
+  var units = 'imperial';
 
+  // will check if input is numeric
   function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
   }
 
-  $('#search').on('click', function(e) {
+  if (isNumeric(q)) {
+    url + '?zip=' + q + '&units=' + units + '&appid=' + apiKey
+  }
+
+  $('.form-control').on('focus', function () {
+    $('.form-control').val('');
+  });
+  $('#search-form').on('submit', function(e) {
     e.preventDefault();
     console.log('form submitted');
-    weatherDisplay.html('loading......');
 
-    console.log(searchText);
+    q = searchText.val();
+    console.log(q);
+    getCurrentWeather();
     // if (isNumeric(data.q)) {
     //   url = "http://api.openweathermap.org/data/2.5/weather" + data.zip;
     // }
 
   });
 
-  var getCurrentWeather = function () {
+  function getCurrentWeather() {
     $.getJSON(
       url,
       function (data) {
         console.log(data);
+        weatherDisplay.html(data.city.name);
     });
   }
 });
